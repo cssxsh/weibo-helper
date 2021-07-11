923e12c7237e85b9ef286b11fc66eeb8447d8f1b
cssxsh <cssxsh@gmail.com>
2021-07-11 14:11:08 +0800

    feat: WeiboEmoticonData default

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/data/WeiboEmoticonData.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/data/WeiboEmoticonData.kt
@@ -1,9 +1,23 @@
 package xyz.cssxsh.mirai.plugin.data
 
+import kotlinx.serialization.decodeFromString
 import net.mamoe.mirai.console.data.AutoSavePluginData
+import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
+import xyz.cssxsh.weibo.WeiboClient
 import xyz.cssxsh.weibo.data.Emoticon
 
 object WeiboEmoticonData : AutoSavePluginData("WeiboEmoticonData") {
-    val emoticons by value(mutableMapOf<String, Emoticon>())
+
+    internal fun default() = this::class.java.getResourceAsStream("Emoticons.json").use {
+        val text = requireNotNull(it) { "找不到Emoticons初始化文件" }.reader().readText()
+        WeiboClient.Json.decodeFromString<List<Emoticon>>(text)
+    }
+
+    @ValueDescription("表情数据")
+    val emoticons: MutableMap<String, Emoticon> by value {
+        if (isEmpty()) {
+            default().forEach { put(it.phrase, it) }
+        }
+    }
 }
