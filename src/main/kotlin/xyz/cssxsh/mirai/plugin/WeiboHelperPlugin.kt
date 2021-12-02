dca4eea18d4d47a346aff573e884825ea9ee95a2
cssxsh <cssxsh@gmail.com>
2021-12-02 21:38:08 +0800

    build: 1.4.1

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/WeiboHelperPlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/WeiboHelperPlugin.kt
@@ -13,7 +13,7 @@ import xyz.cssxsh.mirai.plugin.command.*
 import xyz.cssxsh.mirai.plugin.data.*
 
 object WeiboHelperPlugin : KotlinPlugin(
-    JvmPluginDescription("xyz.cssxsh.mirai.plugin.weibo-helper", "1.4.0") {
+    JvmPluginDescription("xyz.cssxsh.mirai.plugin.weibo-helper", "1.4.1") {
         name("weibo-helper")
         author("cssxsh")
     }
