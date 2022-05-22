8bfc2c3631652deb39ca5519cc83b38b89b650c6
cssxsh <cssxsh@gmail.com>
2022-05-22 13:29:43 +0800

    update: mirai 2.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
 
-    id("net.mamoe.mirai-console") version  "2.10.3"
+    id("net.mamoe.mirai-console") version  "2.11.0"
     id("net.mamoe.maven-central-publish") version "0.7.1"
 }
 
@@ -15,6 +15,7 @@ mavenCentralPublish {
     licenseFromGitHubProject("AGPL-3.0", "master")
     publication {
         artifact(tasks.getByName("buildPlugin"))
+        artifact(tasks.getByName("buildPluginLegacy"))
     }
 }
 
@@ -23,16 +24,8 @@ repositories {
     mavenCentral()
 }
 
-kotlin {
-    sourceSets {
-        all {
-            languageSettings.optIn("kotlin.RequiresOptIn")
-        }
-    }
-}
-
 dependencies {
-    implementation("io.ktor:ktor-client-encoding:1.6.5") {
+    implementation("io.ktor:ktor-client-encoding:1.6.7") {
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
         exclude(group = "org.slf4j")
@@ -40,10 +33,10 @@ dependencies {
     }
     implementation("org.jclarion:image4j:0.7")
     implementation("org.apache.commons:commons-text:1.9")
-    compileOnly("net.mamoe:mirai-core-utils:2.10.3")
+    compileOnly("net.mamoe:mirai-core-utils:2.11.0")
 
     testImplementation("net.mamoe.yamlkt:yamlkt-jvm:0.10.2")
-    testImplementation(kotlin("test", "1.6.0"))
+    testImplementation(kotlin("test", "1.6.21"))
 }
 
 tasks {
