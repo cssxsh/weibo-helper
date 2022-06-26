d733f899f981308e2f7d2021e962f7d1e1a67ea0
cssxsh <cssxsh@gmail.com>
2022-06-26 21:27:37 +0800

    add: mirai console version check

--- src/main/kotlin/xyz/cssxsh/mirai/weibo/WeiboHelperPlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/weibo/WeiboHelperPlugin.kt
@@ -1,10 +1,13 @@
 package xyz.cssxsh.mirai.weibo
 
 import kotlinx.coroutines.*
+import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
 import net.mamoe.mirai.console.extension.*
 import net.mamoe.mirai.console.plugin.jvm.*
+import net.mamoe.mirai.console.plugin.*
+import net.mamoe.mirai.console.util.*
 import net.mamoe.mirai.utils.*
 import xyz.cssxsh.mirai.weibo.command.*
 import xyz.cssxsh.mirai.weibo.data.*
@@ -38,6 +41,11 @@ object WeiboHelperPlugin : KotlinPlugin(
     }
 
     override fun onEnable() {
+        // XXX: mirai console version check
+        check(SemVersion.parseRangeRequirement(">= 2.12.0-RC").test(MiraiConsole.version)) {
+            "$name $version 需要 Mirai-Console 版本 >= 2.12.0，目前版本是 ${MiraiConsole.version}"
+        }
+
         WeiboTaskData.reload()
         WeiboHelperSettings.reload()
         WeiboHelperSettings.save()
