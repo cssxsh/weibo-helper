a9ad3c0002b08406edcaf58acf3048ff9d14d657
cssxsh <cssxsh@gmail.com>
2022-10-18 17:51:44 +0800

    fix: emoticon

--- src/main/kotlin/xyz/cssxsh/mirai/weibo/data/WeiboEmoticonData.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/weibo/data/WeiboEmoticonData.kt
@@ -6,23 +6,18 @@ import net.mamoe.mirai.console.data.*
 import net.mamoe.mirai.console.util.*
 import xyz.cssxsh.weibo.*
 import xyz.cssxsh.weibo.data.*
+import java.io.*
 
 object WeiboEmoticonData : AutoSavePluginData("WeiboEmoticonData") {
 
-    @OptIn(ExperimentalSerializationApi::class)
-    internal fun default() = this::class.java.getResourceAsStream("Emoticons.json").use {
-        WeiboClient.Json.decodeFromStream<List<Emoticon>>(requireNotNull(it) { "找不到Emoticons初始化文件" })
+    internal fun default(): Map<String, Emoticon> {
+        val url = this::class.java.getResource("Emoticons.json") ?: throw FileNotFoundException("Emoticons.json")
+        return WeiboClient.Json.decodeFromString(url.readText())
     }
 
     @ConsoleExperimentalApi
     override fun shouldPerformAutoSaveWheneverChanged(): Boolean = false
 
     @ValueDescription("表情数据")
-    val emoticons: MutableMap<String, Emoticon> by value {
-        if (isEmpty()) {
-            for (item in default()) {
-                put(item.phrase, item)
-            }
-        }
-    }
+    val emoticons: MutableMap<String, Emoticon> by value { putAll(default()) }
 }
